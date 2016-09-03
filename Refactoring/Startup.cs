3e3e5e2a7e121d8cd2358729612eb0224e99ce44
terje <terje@MBP-WIN>
2016-09-03 16:58:04 +0200

    Add project files.

---
+++ Refactoring/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Refactoring.Startup))]
+namespace Refactoring
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
